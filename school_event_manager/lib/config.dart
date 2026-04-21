d626b48739e23413686b0860f6da58529666af91
Mark <markilagan2005@gmail.com>
2026-04-22 04:42:15 +0800

    chore: point app API to live Render service

--- school_event_manager/lib/config.dart
+++ school_event_manager/lib/config.dart
@@ -2,10 +2,10 @@ const int attendanceTimeoutMinutes = 60;
 
 const String apiBaseUrlDefault = String.fromEnvironment(
   'API_BASE_URL',
-  defaultValue: 'http://10.0.2.2:3000/api',
+  defaultValue: 'https://school-event-managements.onrender.com',
 );
 
 const bool lockServerSettings = bool.fromEnvironment(
   'LOCK_SERVER_SETTINGS',
-  defaultValue: false,
+  defaultValue: true,
 );
